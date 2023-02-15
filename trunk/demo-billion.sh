3c2f3fce5058ebeed408e4a0e93a5e9491d7d07a
Collin Beaudoin <beaudoco@mail.gvsu.edu>
2023-02-15 11:22:22 -0500

    adding compiled versions of programs as well as the result of the billion word file for cat, she, like and a sentence of 'cat she like'

---
+++ trunk/demo-billion.sh
@@ -0,0 +1,21 @@
+make
+# This function will convert text to lowercase and remove special characters
+normalize_text() {
+  awk '{print tolower($0);}' | sed -e "s/’/'/g" -e "s/′/'/g" -e "s/''/ /g" -e "s/'/ ' /g" -e "s/“/\"/g" -e "s/”/\"/g" \
+  -e 's/"/ " /g' -e 's/\./ \. /g' -e 's/<br \/>/ /g' -e 's/, / , /g' -e 's/(/ ( /g' -e 's/)/ ) /g' -e 's/\!/ \! /g' \
+  -e 's/\?/ \? /g' -e 's/\;/ /g' -e 's/\:/ /g' -e 's/-/ - /g' -e 's/=/ /g' -e 's/=/ /g' -e 's/*/ /g' -e 's/|/ /g' \
+  -e 's/«/ /g' | tr 0-9 " "
+}
+
+mkdir word2vec
+cd word2vec
+
+wget http://www.statmt.org/lm-benchmark/1-billion-word-language-modeling-benchmark-r13output.tar.gz
+tar -xvf 1-billion-word-language-modeling-benchmark-r13output.tar.gz
+for i in `ls 1-billion-word-language-modeling-benchmark-r13output/training-monolingual.tokenized.shuffled`; do
+  normalize_text < 1-billion-word-language-modeling-benchmark-r13output/training-monolingual.tokenized.shuffled/$i >> data.txt
+done
+
+
+time ./word2vec -train data.txt -output vectors.bin -cbow 1 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 20 -binary 1 -iter 15
+./distance vectors.bin
